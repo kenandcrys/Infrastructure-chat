d7b312233355889961fd9f2ffb1bf1e7c0b771bd
Fish <kenandcrys@live.com>
2024-04-23 11:39:56 -0500

    Update next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "utfs.io"
-        ]
-    }
+        remotePatterns: [
+            {
+                hostname: "utfs.io",
+                
+            },
+        ],
+    },
 };
 
 export default nextConfig;
